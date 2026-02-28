df2a19eeaa484cf6d749be3053fd20046dce98c5
Vodangkydev <165755675+Vodangkydev@users.noreply.github.com>
2026-02-28 11:17:09 +0700

    Update AdminLogin.jsx

--- front-end/src/admin/auth/AdminLogin.jsx
+++ front-end/src/admin/auth/AdminLogin.jsx
@@ -73,6 +73,7 @@ const AdminLogin = () => {
                   name="email"
                   type="email"
                   required
+                  placeholder="admin@gmail.com"
                   className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 bg-white text-gray-900 focus:bg-white active:bg-white focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                   value={username}
                   onChange={(e) => setUsername(e.target.value)}
@@ -90,6 +91,7 @@ const AdminLogin = () => {
                   name="password"
                   type="password"
                   required
+                  placeholder="123456"
                   className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 bg-white text-gray-900 focus:bg-white active:bg-white focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
